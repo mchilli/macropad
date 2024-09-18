20bf889509c1a2247d5237c1fa9292d00dfe97a8
mchilli <mchilli@web.de>
2024-09-18 12:45:01 +0200

    reorganize files

--- circuitpython-9.x/utils/utils.py
+++
@@ -1,8 +0,0 @@
-def to_chunks(lst, n):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-def center(string:str, width:int = 0, sep:str = ' '):
-    left = (width - len(string)) // 2
-    right = width - len(string) - left
-    return f"{sep * left}{string}{sep * right}"
