45694a92cb48777e6878f41f58234fe0f662d192
mchilli <mchilli@web.de>
2024-10-28 11:43:02 +0100

    move file exist function to utils

--- circuitpython/utils/utils.py
+++ circuitpython/utils/utils.py
@@ -1,3 +1,5 @@
+import os
+
 def to_chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
@@ -6,3 +8,10 @@ def center(string:str, width:int = 0, sep:str = ' '):
     left = (width - len(string)) // 2
     right = width - len(string) - left
     return f"{sep * left}{string}{sep * right}"
+
+def path_exist(path):
+    try:
+        os.stat(path)
+        return True
+    except OSError:
+        return False
