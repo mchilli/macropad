66303490b940b6ca0247167d88347a8ab1ee5bf9
mchilli <mchilli@web.de>
2024-11-03 11:42:59 +0100

    update version

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -25,7 +25,7 @@ from utils.utils import get_audio_files
 gc.enable()
 supervisor.runtime.autoreload = False
 
-VERSION = "1.4.0"
+VERSION = "1.4.1"
 # The file in which the settings are saved
 SETTINGSFILE = "settings.json"
 # The file in which the macros are saved
